f38c32f6ece3334ac28be649e569875a5802eed5
pi-nuts <25.n.taira.nutfes@gmail.com>
2025-10-06 21:56:42 +0900

    Revert "[add] add TicketCard componet(#19)"
    This reverts commit ae17ef5922d4753b06c5443275c0b9328a09070f.

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,17 +1,13 @@
 import { BrowserRouter as Router, Routes, Route} from "react-router-dom";
-import TicketCard from "./components/atomic/TicketCard";
+import LogoIcon from "./pages/LogoIcon";
 
 function App() {
-  const handleClick = () => alert('テストボタンが押されました')
   return (
-    <div>
-      <TicketCard
-        title="テストイベント"
-        time="12:00～"
-        location="テスト会場"
-        onClick={handleClick}
-      />
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/logo" element={<LogoIcon />} />
+      </Routes>
+    </Router>
   );
 }
 
--- web/src/pages/LogoIcon.tsx
+++ web/src/pages/LogoIcon.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import LogoIconComponent from '../components/atomic/LogoIcon';
 
-const images = () => {
+const LogoIcon = () => {
   const handleLogo = () => {
     alert('25th-ロゴ');
   };
@@ -18,4 +18,4 @@ const images = () => {
   );
 };
 
-export default images;
+export default LogoIcon;

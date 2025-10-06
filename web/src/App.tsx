329f013d6e60e5c5f709c16abb4dab89b359800f
FastDefence <aaa100@outlook.jp>
2025-10-06 17:50:45 +0900

    [fix] ボタンだけ、 (19)

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,17 +1,15 @@
 import { BrowserRouter as Router, Routes, Route} from "react-router-dom";
 import Login from "./pages/login"
-import Label from "./components/atomic/Label"
+import Button from "./components/atomic/Button"
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/login" element={<Login />} />
-        <Route path="/TicketList" element={<TicketList />} />
-        <Route path="/label" element={<Label text = "登録"/>} />
+        <Route path="/button" element={<Button label="登録" variant="registration"/>} />
       </Routes>
     </Router>
-      
   )
 }
 

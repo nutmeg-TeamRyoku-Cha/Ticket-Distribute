cce97130e171f176977c54d452ffef413215c774
FastDefence <aaa100@outlook.jp>
2025-10-06 22:33:27 +0900

    [fix] おかたづけ (13)

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,25 +1,10 @@
 import { BrowserRouter as Router, Routes, Route} from "react-router-dom";
-import React, { useState } from "react";
-import InputField from "./components/atomic/InputField";
 
 function App() {
-  const [value, setValue] = useState("");
-
   return (
     <Router>
       <Routes>
-        <Route
-          path="/input"
-          element={
-            <InputField
-              label="ユーザー名"
-              placeholder="山田太郎"
-              value={value}
-              onChange={(e) => setValue(e.target.value)}
-              type="text"
-            />
-          }
-        />
+        <Route></Route>
       </Routes>
     </Router>
   )

cc5c7d55f2e20f7bf6b57184c3538b0ab4d5068e
namagai <25.k.ikegai.nutfes@gmail.com>
2025-10-06 12:32:17 +0900

    [fix] App.tsxを修正(13)

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,6 +1,7 @@
 import { BrowserRouter as Router, Routes, Route} from "react-router-dom";
-import Login from "./pages/login"
+import Login from "./pages/login";
 import Label from "./components/atomic/Label";
+import InputField from "./components/atomic/InputField";
 
 function App() {
   return (

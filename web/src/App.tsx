f9ee58295f85f74400550b24470d47eca2101a4f
FastDefence <aaa100@outlook.jp>
2025-10-09 02:16:21 +0900

    [fix] ページ遷移

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route} from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
 
 //Pageのみ配置
 import LoginPage from "./pages/Account";
@@ -10,11 +10,12 @@ function App() {
   return (
     <Router>
       <Routes>
+        <Route path="/" element={<Navigate to="/account" replace />} />
         <Route path="/account" element={<LoginPage />} />
         <Route path="/ticket" element={<TicketList />} />
         <Route path="/event" element={<EventList />} />
         <Route path="/getticket" element={<GetTicket />} />
-        <Route path="/event" element={<EventList />} />
+        <Route path="*" element={<Navigate to="/account" replace />} />
       </Routes>
     </Router>
   );
